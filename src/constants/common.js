de560b5a9287c58a6410af72c62d2dd8c77e8696
bgutsol <bogdan.gutsol@gmail.com>
2018-09-03 01:56:09 +0300

    build fix

--- src/constants/common.js
+++ src/constants/common.js
@@ -1,1 +1,4 @@
 export const INSTAGRAM_CLIENT_ID = 'daad44e65eb14a50b73c6adcf23262a4';
+
+// eslint-disable-next-line no-undef
+export const REDIRECT_URL = process.env.NODE_ENV === 'production' ? 'https://tag-picker.netlify.com/' : __webpack_public_path__;
--- src/containers/LoginContainer/LoginContainer.js
+++ src/containers/LoginContainer/LoginContainer.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import {INSTAGRAM_CLIENT_ID} from 'Constants/common';
+import {INSTAGRAM_CLIENT_ID, REDIRECT_URL} from 'Constants/common';
 import Login from 'Components/Login';
 
 export class InstagramLogin extends React.Component {
   handleLoginClick() {
-    // eslint-disable-next-line no-undef
-    const redirectUrl = __webpack_public_path__;
-
-    window.location.href = `https://api.instagram.com/oauth/authorize/?client_id=${INSTAGRAM_CLIENT_ID}&redirect_uri=${redirectUrl}&scope=public_content&response_type=token`
+    window.location.href = `https://api.instagram.com/oauth/authorize/?client_id=${INSTAGRAM_CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=public_content&response_type=token`
   }
 
   render() {
