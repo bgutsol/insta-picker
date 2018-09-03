06420a4b54c338193c7c7a63c93244c7b302eb5b
bgutsol <bogdan.gutsol@gmail.com>
2018-09-03 03:55:02 +0300

    update about page

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -6,11 +6,10 @@ const AboutPage = () => {
     <div className='about-page'>
       <div className="about-page__text rich-text">
         <h2>WTF</h2>
-        <p>Through this app you can search recently added media with definite
-          hashtag. It's using Instagram public API for fetching media. Now App is in a SANDBOX MODE, so you cannot
-          easily authorize and use it, because of Instagram API
-          restrictions. So, to test it you have to contact me and I will add you as a sandbox user. Then you will be
-          able to authorize and fetch your account media.</p>
+        <p>Through this app you can search recently added media with a certain hashtag. It's using Instagram public API
+          for fetching media. Now App is in a SANDBOX MODE, so you cannot easily authorize and use it, because of
+          Instagram API restrictions. So, for using it, you have to contact me and I will add you as a sandbox user.
+          Then you will be able to authorize and fetch your account media.</p>
         <p>Please, note, that you need to have some recently added
           photos, with tags, in your account, to be able to search among them.</p>
       </div>
