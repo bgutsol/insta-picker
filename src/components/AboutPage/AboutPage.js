dc85754007578969b26aa624c0bf14dd2fc3678b
bgutsol <bogdan.gutsol@gmail.com>
2018-09-03 04:01:18 +0300

    linter fix

--- src/components/AboutPage/AboutPage.js
+++ src/components/AboutPage/AboutPage.js
@@ -6,7 +6,7 @@ const AboutPage = () => {
     <div className='about-page'>
       <div className="about-page__text rich-text">
         <h2>WTF</h2>
-        <p>Through this app you can search recently added media with a certain hashtag. It's using Instagram public API
+        <p>Through this app you can search recently added media with a certain hashtag. It&apos;s using Instagram public API
           for fetching media. Now App is in a SANDBOX MODE, so you cannot easily authorize and use it, because of
           Instagram API restrictions. So, for using it, you have to contact me and I will add you as a sandbox user.
           Then you will be able to authorize and fetch your account media.</p>
